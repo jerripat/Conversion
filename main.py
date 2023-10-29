6205872219fc0394c85605cab9c43e715d6f6813
jerripat <jerripatterson@gmail.com>
2023-10-29 16:30:52 -0500

    October29 Update

--- main.py
+++ main.py
@@ -16,32 +16,19 @@ radioGrp = IntVar ()
 radioGrp.set ( 1 )
 usrname = os.getlogin ()
 username = string.capwords ( usrname )
-dataFrame = Frame ( tk )
-lblOne = (Label ( dataFrame, text = "Lets collect some Data!!", width = "400", height = "350", font = ("Arial", 16),
-                  bg = formBgColor ))
 
-# First frame
-frame_one = Frame(tk, width=200, height=200,bd=5, bg=formBgColor)
-#frame_one.place(relx=0.5,rely=0.5,anchor=CENTER)
-frame_one_label = Label(frame_one, text="Enter your weight", font=("Arial",12),bg='white',fg='black')
-#frame_one_label.place(relx=0.5,rely=0.25,anchor=CENTER)
+# Define a menu
+my_menu = Menu ( tk )
+tk.config ( menu = my_menu )
+file_menu = Menu ( my_menu, tearoff = 0 )
+my_menu.add_cascade ( label = "File", menu = file_menu )
+file_menu.add_command ( label = "New" )
+file_menu.add_command ( label = "Exit", command = tk.quit )
+dataframe = Frame( tk, bg = formBgColor)
+dataFrame = LabelFrame ( tk, text = "Data", bg = formBgColor)
 
+def getUser():
 
-def fakeCommand():
-
-    pass
-
-
-def hide():
-   pass
-
-def show():
-    pass
-
-def getUser(userId):
-    wht = 0
-    hgt = 0.0
-    time = 0.0
     if userId == 1:
         lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )  # Placed at the top center
         txtUser = Entry ( tk, font = ("Arial", 12), bg = formBgColor, fg = formFgColor )
@@ -49,26 +36,23 @@ def getUser(userId):
 
 
 def radioChoice():
-    # btnStart.destroy()
-    # lblMarquee.destroy()
-
     if radioGrp.get () == 1:
-        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )  # Placed at the top center
+        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )
     elif radioGrp.get () == 2:
-        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )  # Placed at the top center
+        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )
     else:
-        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )  # Placed at the top center
+        lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )
 
 
 # This is the greeting Label
 lblMarquee = Label ( tk, text = f"Welcome to the Calorie Counter {username}",
                      bg = formBgColor, fg = formFgColor, font = ("Arial", 16) )
 lblMarquee.place ( relx = 0.5, rely = 0.1, anchor = N )  # Placed at the top center
-
+lblAsk = (Label ( tk, text = "Would you like to start a log session?", font = ("Arial", 12), bg = formBgColor,
+                  fg = formFgColor ))
 radYes = Radiobutton ( tk, text = "Yes", bg = radioGrpBg, fg = btnColor, variable = radioGrp, value = 1 )
 radNo = Radiobutton ( tk, text = "No", bg = radioGrpBg, fg = btnColor, variable = radioGrp, value = 2 )
 radElse = Radiobutton ( tk, text = "Someone else", bg = radioGrpBg, fg = btnColor, variable = radioGrp, value = 3 )
-btnSubmit = Button ( tk, text = "Submit", command = getUser, font = ("Mono", 11), width = 6, height = 1 )
 lblAskWeight = (Label ( tk, text = "Please enter your weight", font = ("Arial",) ))
 lblAskHeight = (Label ( tk, text = "Please enter your height", font = ("Arial",) ))
 lblAskTime = (Label ( tk, text = "Please enter your time", font = ("Arial",) ))
@@ -77,36 +61,36 @@ entHeight = Entry ( tk, font = ("Arial", 12), bg = formBgColor )
 entTime = Entry ( tk, font = ("Arial", 12), bg = formBgColor )
 
 
+def openDataFrame():
+    lblAsk.place_forget ()
+    radYes.place_forget ()
+    radNo.place_forget ()
+    radElse.place_forget ()
+    lblMarquee.place_forget ()
+    btnStart.place_forget ()
+    dataFrame.place ()  # Placed at the top center
+    lblOne = Label ( dataFrame, text = "Lets collect some Data!!", font = ("Arial", 16),
+                  bg = formBgColor, fg = formFgColor )
+    lblOne.place ( relx = 0.5, rely = 0.5, anchor = N )
+    btnDataSubmit = Button ( dataFrame, text = "Submit", command = openDataFrame,
+                             font = ("Arial", 11) )
+    btnDataSubmit.place ( relx = 0.5, rely = 0.7, anchor = N )
+
 def start():
-    lblAsk = (Label ( tk, text = "Would you like to start a log session?", font = ("Arial", 12), bg = formBgColor,
-                      fg = formFgColor ))
     lblAsk.place ( relx = 0.5, rely = 0.5, anchor = CENTER )  # Placed at the top center
     radYes.place ( relx = 0.3, rely = 0.55, anchor = N )
     radNo.place ( relx = 0.4, rely = 0.55, anchor = N )
     radElse.place ( relx = 0.6, rely = 0.55, anchor = N )
-    btnSubmit.place ( relx = 0.5, rely = 0.7, anchor = N )  # Placed at the top center
+    btnSubmit = Button ( tk, text = "Submit", command = openDataFrame, font = ("Arial", 11) )
+    btnSubmit.place ( relx = 0.4, rely = 0.7 , anchor=N)
 
 
-# Define a menu
-my_menu = Menu ( tk )
-tk.config ( menu = my_menu )
-
-# Create menu items
-file_menu = Menu ( my_menu, tearoff = 0 )
-
-my_menu.add_cascade ( label = "File", menu =file_menu)
-file_menu.add_command ( label = "New", command = fakeCommand )
-file_menu.add_command ( label = "Exit", command = tk.quit )
-
-# Create Edit menu items
-edit_menu = Menu ( my_menu, tearoff = 0 )
-my_menu.add_cascade ( label = "Edit", menu =edit_menu)
-edit_menu.add_command ( label = "Cut", command = fakeCommand )
-edit_menu.add_command ( label = "Exit", command = tk.quit )
+  #
 
 
-
-btnStart = Button ( tk, text = "Start", command = start, width = 5, height = 1, font = ("Arial", 11) )
+btnStart = Button( tk, text = "Start", command = start, font = ("Arial", 11) )
 btnStart.place ( relx = 0.5, rely = 0.28, anchor = N )  # Placed at the top center
+
+
 
 tk.mainloop ()
